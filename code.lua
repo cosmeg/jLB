34c4d0302f5e4bdc7be6ad77d873083e4da436e0
John Cramp <john>
2012-12-02 17:59:18 -0800

    bars

--- code.lua
+++ code.lua
@@ -1,7 +1,23 @@
+-- XXX move these addon globals somewhere else?
+-- should my "addon object" be different from this frame?
+local f = CreateFrame("frame")
+local candy = LibStub("LibCandyBar-3.0")
+local barTexture = "Interface\\AddOns\\SharedMedia\\statusbar\\Flat"
 local PLAYER_GUID = UnitGUID("player")
 
-local f = CreateFrame("frame")
-f.bombs = {}
+local function main()
+  f.bars = {}
+
+  candy.RegisterCallback(f, "LibCandyBar_Stop")
+
+  f:SetScript("OnEvent", f.CLEU)
+  -- what units does this get?
+  --f:RegisterEvent("UNIT_AURA")
+
+  -- would I need to use unfiltered?
+  -- is this better or worse than UNIT_AURA?
+  f:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+end
 
 
 -- I care about: lb applications, lb removals, deaths, lb explosions?
@@ -12,14 +28,13 @@ function f:CLEU(eventType, ...)
 
   if sourceGUID ~= PLAYER_GUID then return end
 
-  tprint(self.bombs)
-
   if event == "SPELL_AURA_APPLIED" then
-    local spellID, spellName, spellSchool = select(12, ...)
+    local spellID, spellName, spellSchool, auraType, amount = select(12, ...)
     if spellName == "Living Bomb" then
       print(event)
       print(spellName)
-      tinsert(self.bombs, "bomb!")
+      -- TODO factor out
+      self:ShowBar(11.50, destGUID)  -- TODO real duration
     end
 
   elseif event == "SPELL_AURA_REMOVED" then
@@ -27,37 +42,66 @@ function f:CLEU(eventType, ...)
     if spellName == "Living Bomb" then
       print(event)
       print(spellName)
-      tremove(self.bombs)
     end
   end
-
   
   --local name, rank, icon, count, dispelType, duration, expires, caster, isStealable, shouldConsolidate, spellID, canApplyAura, isBossDebuff, value1, value2, value3 = UnitDebuff(unitID, "Living Bomb")
   --local value2 = select(15, UnitDebuff("target", "Ignite"))
-
-  -- how can I differentiate debuffs?
-  -- XXX how can I have inline notes?
-
-
 end
 
 
-f:SetScript("OnEvent", f.CLEU)
--- what units does this get?
---f:RegisterEvent("UNIT_AURA")
+--/run bar=LibStub("LibCandyBar-3.0"):New("Interface\\AddOns\\SharedMedia\\statusbar\\Flat",100,16);bar:SetDuration(600);bar:SetPoint("BOTTOMLEFT");bar:Start()
+--/run bar:ClearAllPoints();bar:SetPoint("TOPLEFT", WorldFrame, "CENTER",400,-300)
+function f:PositionBars()
+  -- XXX get rid of this sort? strictly speaking we know the order already
+  local function BarSorter(a, b)
+    return a.remaining < b.remaining
+  end
+  local sorted = {}
+  for k,bar in pairs(self.bars) do
+    sorted[#sorted + 1] = bar
+  end
+  table.sort(sorted, BarSorter)
 
--- would I need to use unfiltered?
--- is this better or worse than UNIT_AURA?
-f:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+  for i,bar in ipairs(sorted) do
+    bar:ClearAllPoints()
+    bar:SetPoint("TOPLEFT", WorldFrame, "CENTER", 400, -300 + i * 16)
+  end
+end
 
 
+-- TODO move config from here maybe
+-- TODO handle refreshing the dot
+function f:ShowBar(t, destGUID)
+  local bar = candy:New(barTexture, 150, 16)
+  bar:SetLabel("bomb!")
+  bar:SetDuration(t)
+  -- TODO font
 
--- XXX make local
-function tprint(t)
+  bar:Set("jlb:destguid", destGUID)
+  self.bars[destGUID] = bar
+
+  bar:Start()
+  self:PositionBars()
+end
+
+
+--local function BarStopped(event, bar)
+function f:LibCandyBar_Stop(event, bar)
+  print("bar stopped")
+  self.bars[bar:Get("jlb:destguid")] = nil
+end
+    
+
+-- XXX remove
+local function tprint(t)
   local s = "{ "
-  for i,v in ipairs(t) do
+  for k,v in ipairs(t) do
     s = s .. v .. " "
   end
   s = s .. "}"
   print(s)
 end
+
+
+main()
